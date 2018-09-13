1ae123101c1971d097a33fe975006a3b4cfa5b6f
k-kiritaro <pechisuke3@yahoo.co.jp>
2018-09-14 06:17:32 +0900

    added water button, sound

---
+++ Assets/PlayController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayController : MonoBehaviour {
+
+    private AudioSource[] sources;
+
+	// Use this for initialization
+	void Start () {
+        sources = gameObject.GetComponents<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void WaterClicked(){
+        sources[0].Play();
+    }
+}
